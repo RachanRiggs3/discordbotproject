40db466dc1df6d8671f5e24d28edd335bf130f30
Rachan Riggs <vinuchandran110@gmail.com>
2023-12-10 13:50:27 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+import settings
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Ready as {discord.AppInfo.name}")
+    game = discord.Game(name="nothing :(")
+    await bot.change_presence(activity=game)
+
+bot.run(settings.DISCORD_TOKEN)
