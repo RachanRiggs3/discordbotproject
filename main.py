4cbe801e606e3694812038dea8c28f6c540eda56
Rachan Riggs <vinuchandran110@gmail.com>
2023-12-10 16:59:39 +0530

    added avatar command

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Ready as {discord.AppInfo.name}")
+    print(f"Ready as {bot.user}")
     game = discord.Game(name="nothing :(")
     await bot.change_presence(activity=game)
 
 @bot.command()
 async def pingme(ctx):
-    await ctx.send(f"Pong {ctx.mention}")
+    await ctx.send(f"Pong {ctx.author.mention}")
+
+@bot.command()
+async def avatar(ctx, mention=None):
+    if not mention: 
+        useravatarurl = ctx.author.avatar
+        await ctx.send(useravatarurl)
+    else:
+        user = ctx.message.mentions[0] 
+        useravatarurl = user.avatar
+        await ctx.send(useravatarurl)
 
 bot.run(settings.DISCORD_TOKEN)
