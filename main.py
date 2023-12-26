4a1dbd4d8f30b24655341f8d8b27cf1f80532221
Rachan Riggs <vinuchandran110@gmail.com>
2023-12-26 22:11:38 +0530

    added unload reload and load commands

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import settings
 import asyncio
 from discord.ext import commands
-import os
+from os import listdir
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=["!", "."], intents=intents)
@@ -16,11 +16,29 @@ def main():
         await bot.change_presence(activity=game)
 
     async def loading_cogs():
-        for files in os.listdir("./commands"):
+        for files in listdir("./commands"):
             if files.endswith(".py"):
                 await bot.load_extension(f"commands.{files[:-3]}")
 
     asyncio.run(loading_cogs())
+
+    @bot.command()
+    @commands.is_owner()
+    async def load(ctx, filename: str):
+        await bot.load_extension(f"commands.{filename}")
+        await ctx.send(f"Loaded {filename} command")
+    
+    @bot.command()
+    @commands.is_owner()
+    async def unload(ctx, filename: str):
+        await bot.unload_extension(f"commands.{filename}")
+        await ctx.send(f"Unloaded {filename} command")
+    
+    @bot.command()
+    @commands.is_owner()
+    async def reload(ctx, filename : str):
+        await bot.reload_extension(f"commands.{filename}")
+        await ctx.send(f"Reloaded {filename} command")
 
     bot.run(settings.DISCORD_TOKEN)
 
