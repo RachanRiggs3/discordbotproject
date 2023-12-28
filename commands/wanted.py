aa645ab3b88eab373bf8d324f2c8910effe3f7db
Rachan Riggs <vinuchandran110@gmail.com>
2023-12-28 19:26:16 +0530

    added wanted command (its really not optimised and i dont know how i can send images without saving it lol)

---
+++ commands/wanted.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from io import BytesIO
+from PIL import Image
+
+class wanted(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def wanted(self, ctx, user : discord.Member = None):
+        if user == None:
+            user = ctx.author
+        wanted_image = Image.open(fp=r"C:\Users\HP\Desktop\discord py project\command images\wanted.png")
+        user_display_avatar = user.display_avatar
+        user_avatar = BytesIO(await user_display_avatar.read())
+        pfp = Image.open(user_avatar)  
+        
+        pfp = pfp.resize((977, 977)) 
+        wanted_image.paste(pfp, (217, 453))
+        wanted_image.save(fp=r"C:\Users\HP\Desktop\discord py project\command images\command image cache\wanted.png")
+        await ctx.send(file=discord.File(fp=r"C:\Users\HP\Desktop\discord py project\command images\command image cache\wanted.png"))
+
+        
+async def setup(bot: commands.Bot):
+    await bot.add_cog(wanted(bot))
